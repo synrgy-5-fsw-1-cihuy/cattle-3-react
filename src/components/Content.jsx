7608bb495f92c98e837b9181580228cb1ff683c3
Abdul Jalil Nawawi <ajnawawi@gmail.com>
2022-12-07 23:07:44 +0700

    add padding and style on text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -9,15 +9,15 @@ import sayur4 from '../assets/img4.jpg'
 
 const Content = () => {
   return (
-    <Container>
+    <Container >
       <h3>Jenis-Jenis Sayuran yang Menyehatkan dan Wajib Dikonsumsi</h3>
-      <Row>
-        <Col>
+      <Row >
+        <Col className="p-2">
           <Card style={{ width: '30rem' }}>
           <Card.Img variant="top" src={sayur1}/>
             <Card.Body>
-              <Card.Title>Sayur Bayam</Card.Title>
-              <Card.Text>
+              <Card.Title style={{fontFamily:"fantasy", fontWeight:"bold", textAlign:"center"}}>Sayur Bayam</Card.Title>
+              <Card.Text style={{textAlign:"justify"}}>
                 Nama pertama yang pasti tidak asing dalam masyarakat Indonesia adalah bayam. 
                 Sayuran ini digambarkan sebagai makanan yang bisa menguatkan tubuh. 
                 Selain itu, bayam pun punya kandungan yang bisa menyehatkan organ dalam tubuh. 
@@ -27,12 +27,12 @@ const Content = () => {
           </Card>
         </Col>
       
-        <Col>
+        <Col className="p-2">
           <Card style={{ width: '30rem' }}>
           <Card.Img variant="top" src={sayur2} />
             <Card.Body>
-              <Card.Title>Tauge</Card.Title>
-              <Card.Text>
+              <Card.Title style={{fontFamily:"fantasy", fontWeight:"bold", textAlign:"center"}}>Tauge</Card.Title>
+              <Card.Text style={{textAlign:"justify"}}>
                 Jenis sayur yang satu ini pun cukup populer di banyak menu makanan. 
                 Kamu bisa menumis atau merebus tauge untuk dapatkan makanan enak yang menyehatkan. 
                 Tauge pun bisa dijumpai di menu ketoprak, soto, bakso, dan rawon.
@@ -40,13 +40,13 @@ const Content = () => {
             </Card.Body>
           </Card>
         </Col>
-      
-        <Col>
+
+        <Col className="p-2">
           <Card style={{ width: '30rem' }}>
             <Card.Img variant="top" src={sayur3} />
             <Card.Body>
-              <Card.Title>Selada</Card.Title>
-              <Card.Text>
+              <Card.Title style={{fontFamily:"fantasy", fontWeight:"bold", textAlign:"center"}}>Selada</Card.Title>
+              <Card.Text style={{textAlign:"justify"}}>
                 Kamu bisa mengonsumsi selada tanpa diolah terlebih dulu. 
                 Cukup mencucinya hingga bersih dengan air dingin untuk dapatkan kesegaran dari sayuran ini. 
                 Selada bisa disajikan dengan campuran daging ayam dan sapi. 
@@ -56,12 +56,12 @@ const Content = () => {
           </Card>
         </Col>
         
-        <Col>
+        <Col className="p-2">
           <Card style={{ width: '30rem' }}>
             <Card.Img variant="top" src={sayur4} />
             <Card.Body>
-              <Card.Title>Asparagus</Card.Title>
-              <Card.Text>
+              <Card.Title style={{fontFamily:"fantasy", fontWeight:"bold", textAlign:"center"}}>Asparagus</Card.Title>
+              <Card.Text style={{textAlign:"justify"}}>
                 Mungkin ini sayur paling mahal di antara jenis sayur yang sudah disebutkan di atas. 
                 Namun, kamu bisa mendapatkan banyak hal dalam sayur asparagus. 
                 Dari sisi rasa, asparagus punya rasa yang gurih. Teksturnya pun sangat renyah. 
