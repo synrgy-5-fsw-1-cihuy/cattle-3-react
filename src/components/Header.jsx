517861127bf5034a0128a9757fa7621f7948b5da
Hasan <57561691+hasan-dev@users.noreply.github.com>
2022-12-08 16:15:18 +0700

    add header (#2)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,50 @@
-import Navbar from "react-bootstrap/Navbar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Header = () => {
+function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      Header
+    <Navbar
+      collapseOnSelect
+      expand='lg'
+      variant='dark'
+      style={{ backgroundColor: '#68B984' }}
+    >
+      <Container>
+        <Navbar.Brand
+          href='#home'
+          className='font-weight'
+          style={{
+            marginLeft: '150px',
+            fontWeight: 'bold',
+            fontStyle: 'italic',
+            textShadow: '1px 2px black',
+          }}
+        >
+          Hayur
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav
+            style={{
+              marginLeft: '300px',
+              fontWeight: 'bold',
+              textShadow: '2px 2px gray',
+            }}
+          >
+            <Nav.Link href='#home' style={{ marginRight: '40px' }}>
+              Home
+            </Nav.Link>
+            <Nav.Link href='#contact' style={{ marginRight: '40px' }}>
+              Contact
+            </Nav.Link>
+            <Nav.Link href='#about'>About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
-};
+}
 
 export default Header;
