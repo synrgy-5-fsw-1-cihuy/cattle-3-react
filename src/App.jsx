3534cece596456d2549365f10eae200e7be62c14
lusiannandang <lusiannandang@gmail.com>
2022-12-08 08:27:48 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
-import Carousel from "./components/Carousel";
+import Carousel from "./components/Carousel/Carousel";
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,48 @@
+import Carousel from 'react-bootstrap/Carousel';
+import gambar1 from '../../assets/gambar1.jpeg'
+import styles from './Carousel.module.css'
+
+function UncontrolledExample() {
+  return (
+    <Carousel className={styles.body}>
+      <Carousel.Item className={styles.body}>
+        <img
+          src={gambar1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
